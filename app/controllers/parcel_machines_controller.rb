7921b14a4467190c3b26d621b2523c255771acdf
Zil Norvilis <zilasino27@gmail.com>
2021-11-05 17:02:49 +0200

    Add show page

--- app/controllers/parcel_machines_controller.rb
+++ app/controllers/parcel_machines_controller.rb
@@ -1,4 +1,6 @@
 class ParcelMachinesController < ApplicationController
+  before_action :set_parcel_machine, only: %i[ show ]
+
   def index
     if params['search']
       @parcel_machines = search(params['search'])
@@ -7,10 +9,24 @@ class ParcelMachinesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
+
   def search(q)
     ParcelMachine.where("name LIKE ? OR zip LIKE ? OR a1_name LIKE ?\
       OR a2_name LIKE ? OR a3_name LIKE ? OR a5_name LIKE ? OR a7_name LIKE ?", 
         "%#{q}%", "%#{q}%", "%#{q}%","%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%").page params[:page]
+  end
+
+    # Use callbacks to share common setup or constraints between actions.
+  def set_parcel_machine
+    @parcel_machine = ParcelMachine.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def parcel_machine_params
+    params.fetch(:parcel_machine, {}).permit(:search)
   end
 end
--- app/models/parcel_machine.rb
+++ app/models/parcel_machine.rb
@@ -1,6 +1,6 @@
 class ParcelMachine < ApplicationRecord
 
-  def full_addresses
+  def full_address
     "#{self.a5_name} #{self.a7_name} #{self.a3_name} #{self.a2_name} #{self.a1_name}"
   end
 end
