a48ea3976369b9a3aa51f96da686f18cb1cd8611
Zil Norvilis <zilasino27@gmail.com>
2021-11-05 16:18:53 +0200

    Add search

--- app/controllers/parcel_machines_controller.rb
+++ app/controllers/parcel_machines_controller.rb
@@ -1,5 +1,16 @@
 class ParcelMachinesController < ApplicationController
   def index
-    @parcel_machines = ParcelMachine.page params[:page]
+    if params['search']
+      @parcel_machines = search(params['search'])
+    else
+      @parcel_machines = ParcelMachine.page params[:page]
+    end
+  end
+
+  private
+  def search(q)
+    ParcelMachine.where("name LIKE ? OR zip LIKE ? OR a1_name LIKE ?\
+      OR a2_name LIKE ? OR a3_name LIKE ? OR a5_name LIKE ? OR a7_name LIKE ?", 
+        "%#{q}%", "%#{q}%", "%#{q}%","%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%").page params[:page]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'parcel_machines#index'
+  resources :parcel_machines , only: [:index, :show]
 end
